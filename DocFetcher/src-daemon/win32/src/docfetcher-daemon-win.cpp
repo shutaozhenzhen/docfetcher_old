664aa689609eff7907a858800df56ee86f85ea8b
toniorush <toniorush@ba8b0031-9e73-4bce-8358-a0b2a480ef00>
2009-03-08 17:44:28 +0000

    Check if this is the only instance It is done by creating a mutex with the daemon exe's full path to distinguish various installations of Docfetcher

--- DocFetcher/src-daemon/win32/src/docfetcher-daemon-win.cpp
+++ DocFetcher/src-daemon/win32/src/docfetcher-daemon-win.cpp
@@ -10,6 +10,9 @@
 #include <sstream>
 
 #include "windows.h"
+#include "TCHAR.h"
+
+#include <psapi.h>
 
 #include "jnotify_win32/Logger.h"
 #include "jnotify_win32/Win32FSHook.h"
@@ -17,15 +20,37 @@
 #include "FolderWatcher.h"
 
 
+/**
+ * used by JNotify
+ *
+ */
 extern bool dbg;
+
+/**
+ * Globals
+ *
+ */
 
 HINSTANCE _hInstance;
 HWND _hwndMain;
 Win32FSHook *_win32FSHook;
 
+/**
+ * This module functions
+ *
+ */
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 bool InitInstance();
+bool isDocFectcherRunning();
+BOOL CALLBACK FindDocFetcherWindow( HWND hwnd, LPARAM lParam ) ;
+bool isDocFetcherProcess(DWORD pid);
 
+/**
+ * Entry point
+ *
+ * installs the watches
+ *
+ */
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 		LPTSTR lpCmdLine, int nCmdShow) {
 	UNREFERENCED_PARAMETER(lpCmdLine);
@@ -37,6 +62,31 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	// If the main window cannot be created, terminate the application.
 	if(!InitInstance()){
 		log("InitInstance failed");
+		return 1;
+	}
+
+	// Check if this is the only instance
+	// It is done by creating a mutex with the daemon exe's full path
+	// to distinguish various installations of Docfetcher
+	TCHAR mutex_name [MAX_PATH] = {0};
+	::GetModuleFileName(NULL, mutex_name, MAX_PATH);
+
+	// replace \ by _
+	for(TCHAR* ch = mutex_name; *ch != _T('\0'); ++ch)
+		if(*ch == _T('\\')) *ch = _T('_');
+
+	if(::OpenMutex(MUTEX_ALL_ACCESS, FALSE, mutex_name) != NULL) {
+		log("a daemon is already running");
+		return 1;
+	}
+
+	if(::CreateMutex(NULL, TRUE, mutex_name) == NULL) {
+		log("cannot create unicity mutex");
+		return 1;
+	}
+
+	if(isDocFectcherRunning()) {
+		log("DocFectcher is running !");
 		return 1;
 	}
 
@@ -66,14 +116,12 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	return 0;
 }
 
-//
-//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  PURPOSE:  Processes messages for the main window.
-//
-//  WM_DESTROY	- post a quit message and return
-//
-//
+/**
+ * Messages received by the window
+ * 	WM_DESTROY
+ * 	WM_REMOVE_WATCH
+ *
+ */
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
 	switch (message) {
 	case WM_DESTROY:
@@ -89,12 +137,12 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
 	return 0;
 }
 
-//
-//   FUNCTION: InitInstance(HINSTANCE, int)
-//
-//   PURPOSE: creates main window
-//
-//
+/**
+ * Creates main window
+ *
+ * Necessary to be able to receive messages
+ *
+ */
 bool InitInstance() {
 
 	HICON hDocFetcherIcon = ::ExtractIcon(_hInstance, "DocFetcher.ico", 0);
@@ -137,5 +185,80 @@ bool InitInstance() {
 	//   UpdateWindow(hWnd);
 
 	return true;
+}
+
+/**
+ * Searchs all windows, and finds Docfetcher's
+ *
+ *
+ */
+bool isDocFectcherRunning() {
+    // Get the list of process identifiers.
+
+    DWORD aProcesses[10024], cbNeeded, cProcesses;
+    unsigned int i;
+
+    log("before enum");
+    if ( !::EnumProcesses( aProcesses, sizeof(aProcesses), &cbNeeded ) )
+        return false;
+
+    log("after enum");
+    // Calculate how many process identifiers were returned.
+
+    cProcesses = cbNeeded / sizeof(DWORD);
+
+    // Print the name and process identifier for each process.
+
+    for ( i = 0; i < cProcesses; i++ ){
+        if( aProcesses[i] != 0 ){
+        	if(isDocFetcherProcess( aProcesses[i] )){
+        		return true;
+        	}
+
+        }
+    }
+    return false;
+
+
+//	bool docfethcer_running_is = false;
+//
+//    ::EnumWindows((WNDENUMPROC)FindDocFetcherWindow, (LPARAM) &docfethcer_running_is) ;
+//	return docfethcer_running_is;
+}
+
+bool isDocFetcherProcess(DWORD pid) {
+    // Get a handle to the process.
+
+    HANDLE hProcess = OpenProcess( PROCESS_QUERY_INFORMATION |
+                                   PROCESS_VM_READ,
+                                   FALSE, pid );
+
+    TCHAR path[MAX_PATH+1] = {0};
+	::GetModuleFileNameEx(hProcess, NULL, path, MAX_PATH);
+	log(path);
+	return false;
+}
+
+
+
+/**
+ * Callback passed to EnumWindows
+ *
+ * lParam contains a pointer to a boolean, to be set if Docfetcher window is found
+ *
+ */
+BOOL CALLBACK FindDocFetcherWindow( HWND hwnd, LPARAM lParam ){
+	TCHAR title [MAX_PATH];
+
+	::GetWindowText(hwnd, title, MAX_PATH);
+
+	if(_tcscmp(title, "DocFetcher") == 0) {
+		bool *docfethcer_running_is = (bool*)lParam;
+		*docfethcer_running_is = true;
+		return FALSE;
+	}else{
+		// continue searching
+		return TRUE ;
+	}
 }
 
