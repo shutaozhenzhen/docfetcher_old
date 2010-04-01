f003cdfc9b3eb157c060af7d66b78d3df28b55e8
derkalender <derkalender@ba8b0031-9e73-4bce-8358-a0b2a480ef00>
2010-04-01 17:30:47 +0000

    Corrected fileheader ({year} was substituted by 2010, Copy&Paste failure by derkalender)

--- DocFetcher/src/net/sourceforge/docfetcher/webinterface/server/IWebserver.java
+++ DocFetcher/src/net/sourceforge/docfetcher/webinterface/server/IWebserver.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) ${year} Andreas Kalender
+ * Copyright (c) 2010 Andreas Kalender
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
