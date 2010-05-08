3a1edc2e70b02cdb97eacd726040c9f5d07a93a1
derkalender <derkalender@ba8b0031-9e73-4bce-8358-a0b2a480ef00>
2010-05-08 10:55:08 +0000

    Added the file type filter to the webinterface

--- DocFetcher/src/net/sourceforge/docfetcher/webinterface/managedBeans/DocFetcherSearchBean.java
+++ DocFetcher/src/net/sourceforge/docfetcher/webinterface/managedBeans/DocFetcherSearchBean.java
@@ -1,19 +1,18 @@
 /*******************************************************************************
- * Copyright (c) 2010 Andreas Kalender
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *    Andreas Kalender - initial API and implementation
+ * Copyright (c) 2010 Andreas Kalender All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse
+ * Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html Contributors: Andreas Kalender -
+ * initial API and implementation
  *******************************************************************************/
 
 package net.sourceforge.docfetcher.webinterface.managedBeans;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.faces.model.SelectItem;
 
@@ -21,6 +20,8 @@ import net.sourceforge.docfetcher.enumeration.Filesize;
 import net.sourceforge.docfetcher.model.ResultDocument;
 import net.sourceforge.docfetcher.model.ScopeRegistry;
 import net.sourceforge.docfetcher.model.ScopeRegistry.SearchException;
+import net.sourceforge.docfetcher.parse.Parser;
+import net.sourceforge.docfetcher.parse.ParserRegistry;
 import net.sourceforge.docfetcher.util.UtilFile;
 import edu.emory.mathcs.backport.java.util.Collections;
 
@@ -108,6 +109,30 @@ public class DocFetcherSearchBean {
 	}
 
 	/**
+	 * Returns a list of the available parser
+	 * 
+	 * @return A list of the supported parser
+	 */
+	public List<FileTypeBean> getParser() {
+		final List<FileTypeBean> result = new ArrayList<FileTypeBean>();
+
+		// Creates one instance of the <code>FileTypeBean</code> per
+		// parser
+		final FileTypeBeanFactory factory = FileTypeBeanFactory.getInstance();
+		
+		for (final Parser parser : ParserRegistry.getParsers()) {
+			final FileTypeBean bean = factory.getOrCreateFileTypeBean(parser);
+
+			result.add(bean);
+		}
+		
+		// Sorts the list of given parsers in alphabetic order
+		Collections.sort(result);
+
+		return result;
+	}
+
+	/**
 	 * Getter
 	 * 
 	 * @return The result corresponding to the given search string
@@ -120,7 +145,9 @@ public class DocFetcherSearchBean {
 																	.equals("")) { //$NON-NLS-1$
 				final ResultDocument[] results = ScopeRegistry	.getInstance()
 																.search(this.searchString);
-
+				
+				final List<ResultDocument> selected = this.filterDocuments(results);
+				
 				// TODO Validate filesizes
 				final boolean filterBySize = (this.getMinFileSize() != 0) && (this.getMaxFileSize() != 0);
 
@@ -134,7 +161,7 @@ public class DocFetcherSearchBean {
 														Filesize.valueOf(this.getMaxFileSizeUnit()));
 				}
 
-				for (final ResultDocument document : results) {
+				for (final ResultDocument document : selected) {
 					final SearchResultBean bean = new SearchResultBean(document);
 
 					if (filterBySize) {
@@ -272,7 +299,7 @@ public class DocFetcherSearchBean {
 	 *            The unsorted list of the search results
 	 * @return The ordered list of the search results
 	 */
-	protected List<SearchResultBean> sortResults(final List<SearchResultBean> result) {
+	protected List<SearchResultBean> sortResults(final List<SearchResultBean> result) {		
 		if (this.getSortColumnName() != null) {
 			final Comparator<SearchResultBean> comparator = SearchResultComparatorFactory	.getInstance()
 																							.getComparator(	this.getSortColumnName(),
@@ -294,5 +321,26 @@ public class DocFetcherSearchBean {
 	 */
 	public String startSearch() {
 		return DocFetcherSearchBean.FORWARD_WITH_TABLE;
+	}
+	
+	/**
+	 * TODO Extract this method into a new and independent class (original code was taken from net.sourceforge.docfetcher.view.ResultPanel.ParserFilter)
+	 * 
+	 * @param results The results without filtering
+	 * @return The list of the elements that are selected by the checked file type filters
+	 */
+	protected List<ResultDocument> filterDocuments(final ResultDocument[] results) {
+		Map<String, Boolean> checkStates = new HashMap<String, Boolean> ();
+		for (Parser parser : ParserRegistry.getParsers())
+			checkStates.put(parser.getClass().getSimpleName(), parser.isChecked());
+		
+		List<ResultDocument> selected = new ArrayList<ResultDocument> (results.length);
+		for (ResultDocument doc : results) {
+			Boolean checkState = checkStates.get(doc.getParserName());
+			if (checkState != null && checkState)
+				selected.add(doc);
+		}
+		
+		return selected;
 	}
 }
